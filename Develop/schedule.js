4025461605201d3a33aceb8e9a449617edcd9208
andytheelf <ballew22@gmail.com>
2020-04-18 22:41:25 -0500

    local storage update

--- Develop/schedule.js
+++ Develop/schedule.js
@@ -4,8 +4,38 @@ console.log(date);
 
 $("#currentDay").append(date);
 
+for(i=0; i < localStorage.length;i++) {
+  $("#task" + i).append(localStorage.getItem(localStorage.key(i)));
+}
+
 }); //DOC READY END
 
-function addTask(btnId, taskId) {
+let nowHour24 = moment().format('H');
+let nowHour12 = moment().format('h');
 
-}
+
+function addTask(btnId, taskId) {
+   console.log(btnId, taskId);
+   localStorage.setItem(btnId, taskId);
+   }
+
+   function updateRowColor($hourRow,hour) { 
+  
+      if (test) { console.log("rowColor ",nowHour24, hour); }
+  
+      if ( hour < nowHour24) {
+        // $hourRow.css('')
+        if (test) { console.log("lessThan"); }
+        $hourRow.css("background-color","lightgrey")
+      } else if ( hour > nowHour24) {
+        if (test) { console.log("greaterthan"); }
+        $hourRow.css("background-color","lightgreen")
+      } else {
+        if (test) { console.log("eqaul"); }
+        $hourRow.css("background-color","tomato")
+      }
+    };
+   
+
+
+
